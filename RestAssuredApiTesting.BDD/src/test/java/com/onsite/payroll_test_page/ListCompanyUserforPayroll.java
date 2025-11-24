b8eca43c57606ea329674b2d1c38cb0f8f4fdb26
Akash Pawar <akashpawar19976@gmail.com>
2025-11-24 15:37:37 +0530

    payroll list by company user for staff api test completed

--- RestAssuredApiTesting.BDD/src/test/java/com/onsite/payroll_test_page/ListCompanyUserforPayroll.java
+++ RestAssuredApiTesting.BDD/src/test/java/com/onsite/payroll_test_page/ListCompanyUserforPayroll.java
@@ -24,10 +24,8 @@ public class ListCompanyUserforPayroll {
 	List<Map<String, Object>> allRecord = new ArrayList<>();
 
 	@Test(priority=1)
-	public void payrollList() {
-
-		//int count = 0;
-
+	public void labourPayrollList() {
+		
 		String companyId = "75916659-9cbe-4ca7-812e-181a29229772";
 
 		while(morePages) {
@@ -131,6 +129,128 @@ public class ListCompanyUserforPayroll {
 			}
 			String nextUrl = payrollListResponse.jsonPath().getString("page.next_url");
 			if (nextUrl != null && !nextUrl.isEmpty()) {
+				pageNumber = Integer.parseInt(nextUrl.split("page=")[1].split("&")[0]);
+			} else {
+				break;
+			}
+		}
+		System.out.println("total record collected :" + totalRecord);
+	}
+
+	@Test(priority=2)
+	public void staffPayrollList() {
+		
+		String companyId = "75916659-9cbe-4ca7-812e-181a29229772";
+
+		while(morePages) {
+			System.out.println("validating page number :" + pageNumber);
+
+			Response payrollResponse = 
+
+					given()
+					.baseUri(ApiBasePath.BASE_URL)
+					.header("Authorization", AuthUtils.getToken())
+					.contentType(ContentType.JSON)
+					.queryParam("company_id", companyId)
+					.queryParam("type", "staff")
+					.queryParam("page", pageNumber)
+					.log().uri()
+
+					.when()
+					.get(Payroll_Api.LIST_COMPANYUSER_FOR_PAYROLL)
+
+					.then()
+					.log().all()
+					.extract().response();
+
+			String payrollresponse = payrollResponse.getBody().asString();
+
+			int responseStatusCode = payrollResponse.getStatusCode();
+			String responseMessage = payrollResponse.jsonPath().getString("message");
+
+			if(responseStatusCode == 200) {
+				System.out.println("response status code : " + responseStatusCode 
+						+ ": response message : " + responseMessage);
+			} else if(responseStatusCode == 400 && "No Entry found".equalsIgnoreCase(responseMessage)) {
+				System.out.println("No more pages available : pagination completed");
+				break;
+			} else {
+				Assert.fail("response statsu code missmatch :" + responseStatusCode 
+						+ ": failure message :" + responseMessage);
+			}
+
+			long responseTime = payrollResponse.getTime();
+			if(responseTime < 2000) {
+				System.out.println("actual response time :" + responseTime);
+			} else {
+				Assert.fail("response time is too long : " + responseTime);
+			}
+
+			//List<Map<String, Object>> staffList = new ArrayList<>();
+			List<Map<String, Object>> staffList = payrollResponse.jsonPath().getList("data");
+			if(staffList != null || staffList.isEmpty()) {
+				System.out.println("staffList is empty id response ");
+				break;
+			}
+
+			allRecord.addAll(staffList);
+			totalRecord = totalRecord + staffList.size();
+
+			for(int i=0; i<staffList.size(); i++) {
+				Map<String, Object> item = staffList.get(i);
+
+				String id = (String) item.get("id");
+				String company_id = (String) item.get("company_id");
+				String userType = (String) item.get("type");
+				String user_created_id = (String) item.get("creator");
+				String user_name = (String) item.get("name");
+				String user_id = (String) item.get("user_id");
+				Integer user_hidden_flag = (Integer) item.get("hidden");
+
+				if(id != null || !id.isEmpty()) {
+					System.out.println("user id is not null or empty : " + id);
+				} else {
+					Assert.fail("user id is null or empty + " + id);
+				}
+
+				if(company_id != null || !company_id.isEmpty()) {
+					System.out.println("company id is not empty or null : " + company_id);
+				} else {
+					Assert.fail("company id is null or empty :" + company_id);
+				}
+
+				if("staff".equalsIgnoreCase(user_id)) {
+					System.out.println("staff type is match" + id + " : " + userType);
+				} else {
+					Assert.fail("staff type is missmatch : " + id + " : " + userType);
+				}
+
+				if(user_created_id != null || !user_created_id.isEmpty()) {
+					System.out.println("created id is not null or empty : " + user_created_id);
+				} else {
+					Assert.fail("created id is null or emoty : " + user_created_id);
+				}
+
+				if(user_name != null || !user_name.isEmpty()) {
+					System.out.println("user name is null or empty : " + user_name);
+				} else {
+					Assert.fail("user name is null or empty : " + user_name);
+				}
+
+				if(user_id != null || !user_id.isEmpty()) {
+					System.out.println("user id is null or empty :" + user_id);
+				} else {
+					Assert.fail("user id is null or empty : " + user_id);
+				}
+
+				if(user_hidden_flag != 1) {
+					System.out.println("user hidden flag is 0 : " + user_hidden_flag);
+				} else {
+					Assert.fail("user hidden flag is 1 :" + user_hidden_flag);
+				}
+			}
+			String nextUrl = payrollResponse.jsonPath().getString("next_url");
+			if(nextUrl != null || !nextUrl.isEmpty()) {
 				pageNumber = Integer.parseInt(nextUrl.split("page=")[1].split("&")[0]);
 			} else {
 				break;
